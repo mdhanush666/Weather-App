5e99b9436bfeda0874c97cbea104506a9cbd3755
mdhanush666 <m.dhanush666dhanu@gmail.com>
2024-10-10 18:36:06 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  }
-});
+  plugins: [react()],
+})
